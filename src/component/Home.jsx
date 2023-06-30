3ee1e1da10cde7767574bbaf2ed78e50b7d26c92
20B01A1291 <20b01a1291@svecw.edu.in>
2023-06-30 23:01:20 +0530

    changed bg image

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div className='hero'>
             
             <div className="card text-bg-dark">
-                <img src="/images/bgmart.jpg" className="card-img" alt="Background" height="500px" />
+                <img src="\images\bgmart.jpg" className="card-img" alt="Background" height="500px" />
                 <div className="card-img-overlay " >
                     <div className="container">
                     
